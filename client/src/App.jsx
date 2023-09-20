f9e32764ce64ffcb5b8d4a69031e79f9c7060752
Nik Syahmi <niksyahmiirfan01@gmail.com>
2023-09-21 00:11:36 +0800

    Add routing to components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,18 @@
 import "./App.css";
-import TweetAnalysisForm from "./components/TweetAnalysisForm";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import TweetAnalysisForm from "./components/TweetAnalysisForm.jsx";
+import Auth from "./pages/Auth.jsx";
 
 function App() {
   return (
     <>
-      <TweetAnalysisForm />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/tweet" element={<TweetAnalysisForm />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
