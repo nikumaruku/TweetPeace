7350f0d66f102d5b2c3d504c91fed9e4fc9bb7e7
Nik Syahmi <niksyahmiirfan01@gmail.com>
2023-09-23 00:39:35 +0800

    Install dependencies

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import TweetAnalysisForm from "./components/TweetAnalysisForm.jsx";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+// import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          {/* <Route path="/" element={<Navbar />} /> */}
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/tweet" element={<TweetAnalysisForm />} />
