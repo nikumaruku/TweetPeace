a36c6f1505fad7ddb89d76bb2c5a240bdc7aba4c
Nik Syahmi <niksyahmiirfan01@gmail.com>
2023-09-23 00:38:58 +0800

    Modify sentimentAnalysis module

--- server/module/sentimentAnalysis.js
+++ server/module/sentimentAnalysis.js
@@ -7,6 +7,46 @@ async function isValidTweetUrl(tweetUrl) {
   return tweetUrlPattern.test(tweetUrl);
 }
 
+// async function analyzeTweetContent(tweetUrl) {
+//   // Check if the provided URL is a valid Twitter tweet URL
+//   if (!isValidTweetUrl(tweetUrl)) {
+//     console.error("Invalid tweet URL:", tweetUrl);
+//     return null;
+//   }
+
+//   const browser = await puppeteer.launch({ headless: false });
+//   // const browser = await puppeteer.launch();
+//   const page = await browser.newPage();
+//   const sentiment = new Sentiment();
+
+//   try {
+//     await page.goto(tweetUrl, { waitUntil: "networkidle2", timeout: 0 });
+//     await page.waitForSelector('div[data-testid="tweetText"]', {
+//       timeout: 20000,
+//     });
+//     const tweetContent = await page.$eval(
+//       'div[data-testid="tweetText"]',
+//       (tweet) => tweet.textContent
+//     );
+
+//     // Analyze the tweet content for sentiment
+//     const analysis = sentiment.analyze(tweetContent);
+
+//     // Determine overall sentiment (positive or negative) based on a threshold
+//     const overallSentiment = analysis.score > 0 ? "Positive" : "Negative";
+
+//     return {
+//       ...analysis,
+//       overallSentiment,
+//     };
+//   } catch (error) {
+//     console.error("Error extracting or analyzing tweet content:", error);
+//     return null; // Handle the error as needed
+//   } finally {
+//     await browser.close();
+//   }
+// }
+
 async function analyzeTweetContent(tweetUrl) {
   // Check if the provided URL is a valid Twitter tweet URL
   if (!isValidTweetUrl(tweetUrl)) {
@@ -15,15 +55,12 @@ async function analyzeTweetContent(tweetUrl) {
   }
 
   const browser = await puppeteer.launch({ headless: false });
-  // const browser = await puppeteer.launch();
   const page = await browser.newPage();
   const sentiment = new Sentiment();
 
   try {
+    await page.goto(tweetUrl);
     await page.goto(tweetUrl, { waitUntil: "networkidle2", timeout: 0 });
-    await page.waitForSelector('div[data-testid="tweetText"]', {
-      timeout: 20000,
-    });
     const tweetContent = await page.$eval(
       'div[data-testid="tweetText"]',
       (tweet) => tweet.textContent
@@ -32,12 +69,41 @@ async function analyzeTweetContent(tweetUrl) {
     // Analyze the tweet content for sentiment
     const analysis = sentiment.analyze(tweetContent);
 
-    // Determine overall sentiment (positive or negative) based on a threshold
-    const overallSentiment = analysis.score > 0 ? "Positive" : "Negative";
+    const overallSentiment = analysis.score > 0 ? "Positive" : "Negative"; //Check back the logic
 
+    // Determine the tweet category
+    let tweetCategory = "Green"; 
+    if (analysis.score < 0) {
+      tweetCategory = "Red";
+    }
+
+    // Calculate the percentage of negative words
+    const words = tweetContent.split(/\s+/);
+    let negativeWordCount = 0;
+    const badWords = {};
+
+    words.forEach((word) => {
+      const wordAnalysis = sentiment.analyze(word);
+      if (wordAnalysis.score < 0) {
+        negativeWordCount++;
+        // Store bad words and their occurrences in an object
+        if (badWords[word]) {
+          badWords[word]++;
+        } else {
+          badWords[word] = 1;
+        }
+      }
+    });
+  
     return {
       ...analysis,
       overallSentiment,
+      tweetCategory,
+      negativeWordCount,
+      badWords: Object.entries(badWords).map(([word, count]) => ({
+        word,
+        count,
+      })),
     };
   } catch (error) {
     console.error("Error extracting or analyzing tweet content:", error);
