66232c86b19af81412994dd5a5cd482a13b625ed
Nik Syahmi <niksyahmiirfan01@gmail.com>
2023-09-26 11:21:16 +0800

    Add new routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,27 @@
-import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import TweetAnalysisForm from "./components/TweetAnalysisForm.jsx";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ReportTweet from "./pages/ReportTweet";
-// import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home.jsx";
+import UserDashboard from "./pages/dashboard/UserDashboard.jsx";
+import Settings from "./pages/Settings.jsx";
+import Profile from "./pages/Profile.jsx";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          {/* <Route path="/" element={<Navbar />} /> */}
+          <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/tweet" element={<TweetAnalysisForm />} />
           <Route path="/report" element={<ReportTweet />} />
+          <Route path="/dashboard" element={<UserDashboard />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
       </BrowserRouter>
     </>
