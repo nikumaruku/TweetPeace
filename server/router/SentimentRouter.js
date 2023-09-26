756d7a96b325c8a0644a73abbe7604a54e5b8525
Nik Syahmi <niksyahmiirfan01@gmail.com>
2023-09-27 00:45:36 +0800

    Implement analysed tweet checking

--- server/router/SentimentRouter.js
+++ server/router/SentimentRouter.js
@@ -1,24 +1,45 @@
 import express from "express";
 import analyzeTweetContent from "../module/sentimentAnalysis.js";
+import { AnalysedTweetModel } from "../models/AnalysedTweetModel.js";
 
 const router = express.Router();
 
-router.post("/analyze", async (req, res) => {
+router.post("/analyse", async (req, res) => {
   const tweetUrl = req.body.tweetUrl;
 
   try {
-    // Call the analyzeTweetContent function to perform sentiment analysis
+    const existingTweet = await AnalysedTweetModel.findOne({ tweetLink: tweetUrl });
+    if (existingTweet) {
+      return res
+        .status(400)
+        .json({ error: "Tweet has already been analysed! Try another tweet" });
+    }
+
     const analysisResult = await analyzeTweetContent(tweetUrl);
     if (analysisResult !== null) {
-      // Return the analysis result as JSON
+      const newAnalysedTweet = new AnalysedTweetModel({
+        tweetLink: tweetUrl,
+      });
+
+      await newAnalysedTweet.save();
       res.json({ analysisResult });
-      console.log({analysisResult});
     } else {
-      // Handle the case when analysis fails
       res.status(400).json({ error: "Analysis failed" });
     }
   } catch (error) {
     console.error("Error analyzing tweet:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+router.get("/analysedTweet", async (req, res) => {
+  try {
+    const numTweets = await AnalysedTweetModel.countDocuments();
+
+    res.status(200).json({ numTweets });
+  } catch (error) {
+    console.error("Error fetching tweet URLs:", error);
+
     res.status(500).json({ error: "Internal server error" });
   }
 });
