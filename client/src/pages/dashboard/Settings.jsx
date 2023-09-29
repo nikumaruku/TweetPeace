231682b9680531b410e2544f6cfc1368216bad11
Nik Syahmi <niksyahmiirfan01@gmail.com>
2023-09-30 00:23:57 +0800

    Modify Settings component dir

--- client/src/pages/dashboard/Settings.jsx
+++ client/src/pages/dashboard/Settings.jsx
@@ -1,81 +1,103 @@
-import { useState } from 'react'
-import { Dialog, Switch } from '@headlessui/react'
-import { Bars3Icon } from '@heroicons/react/20/solid'
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { Dialog, Switch } from "@headlessui/react";
+import { Bars3Icon } from "@heroicons/react/20/solid";
 import {
-  BellIcon,
-  CreditCardIcon,
-  CubeIcon,
-  FingerPrintIcon,
-  UserCircleIcon,
-  UsersIcon,
+  ArrowUturnLeftIcon,
+  // BellIcon,
+  // CreditCardIcon,
+  // CubeIcon,
+  // FingerPrintIcon,
+  // UserCircleIcon,
+  // UsersIcon,
   XMarkIcon,
-} from '@heroicons/react/24/outline'
+} from "@heroicons/react/24/outline";
 
 const navigation = [
-  { name: 'Home', href: '/dashboard' },
-  { name: 'Invoices', href: '#' },
-  { name: 'Clients', href: '#' },
-  { name: 'Expenses', href: '#' },
-]
-const secondaryNavigation = [
-  { name: 'General', href: '#', icon: UserCircleIcon, current: true },
-  { name: 'Security', href: '#', icon: FingerPrintIcon, current: false },
-  { name: 'Notifications', href: '#', icon: BellIcon, current: false },
-  { name: 'Plan', href: '#', icon: CubeIcon, current: false },
-  { name: 'Billing', href: '#', icon: CreditCardIcon, current: false },
-  { name: 'Team members', href: '#', icon: UsersIcon, current: false },
-]
+  { name: "Home", href: "/dashboard" },
+  { name: "Invoices", href: "#" },
+  { name: "Clients", href: "#" },
+  { name: "Expenses", href: "#" },
+];
+// const secondaryNavigation = [
+//   { name: 'General', href: '#', icon: UserCircleIcon, current: true },
+//   { name: 'Security', href: '#', icon: FingerPrintIcon, current: false },
+//   { name: 'Notifications', href: '#', icon: BellIcon, current: false },
+//   { name: 'Plan', href: '#', icon: CubeIcon, current: false },
+//   { name: 'Billing', href: '#', icon: CreditCardIcon, current: false },
+//   { name: 'Team members', href: '#', icon: UsersIcon, current: false },
+// ]
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(" ");
 }
 
 export default function Settings() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
-  const [automaticTimezoneEnabled, setAutomaticTimezoneEnabled] = useState(true)
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const [automaticTimezoneEnabled, setAutomaticTimezoneEnabled] =
+    useState(true);
 
   return (
     <>
       <header className="absolute inset-x-0 top-0 z-50 flex h-16 border-b border-gray-900/10">
         <div className="mx-auto flex w-full max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
           <div className="flex flex-1 items-center gap-x-6">
-            <button type="button" className="-m-3 p-3 md:hidden" onClick={() => setMobileMenuOpen(true)}>
+            <button
+              type="button"
+              className="-m-3 p-3 md:hidden"
+              onClick={() => setMobileMenuOpen(true)}
+            >
               <span className="sr-only">Open main menu</span>
               <Bars3Icon className="h-5 w-5 text-gray-900" aria-hidden="true" />
             </button>
-            <img
-              className="h-8 w-auto"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-              alt="Your Company"
-            />
+
+            <Link to={"/dashboard"}>
+              <button type="button">
+                <span className="sr-only">Open main menu</span>
+                <ArrowUturnLeftIcon
+                  className="h-5 w-5 text-gray-900"
+                  aria-hidden="true"
+                />
+              </button>
+            </Link>
+
           </div>
-          <nav className="hidden md:flex md:gap-x-11 md:text-sm md:font-semibold md:leading-6 md:text-gray-700">
+          {/* <nav className="hidden md:flex md:gap-x-11 md:text-sm md:font-semibold md:leading-6 md:text-gray-700">
             {navigation.map((item, itemIdx) => (
               <a key={itemIdx} href={item.href}>
                 {item.name}
               </a>
             ))}
-          </nav>
+          </nav> */}
           <div className="flex flex-1 items-center justify-end gap-x-8">
-            <button type="button" className="-m-2.5 p-2.5 text-gray-400 hover:text-gray-500">
+            {/* <button type="button" className="-m-2.5 p-2.5 text-gray-400 hover:text-gray-500">
               <span className="sr-only">View notifications</span>
               <BellIcon className="h-6 w-6" aria-hidden="true" />
-            </button>
-            <a href="#" className="-m-1.5 p-1.5">
+            </button> */}
+            {/* <a href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">Your profile</span>
               <img
                 className="h-8 w-8 rounded-full bg-gray-800"
                 src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                 alt=""
               />
-            </a>
+            </a> */}
           </div>
         </div>
-        <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+        <Dialog
+          as="div"
+          className="lg:hidden"
+          open={mobileMenuOpen}
+          onClose={setMobileMenuOpen}
+        >
           <div className="fixed inset-0 z-50" />
           <Dialog.Panel className="fixed inset-y-0 left-0 z-50 w-full overflow-y-auto bg-white px-4 pb-6 sm:max-w-sm sm:px-6 sm:ring-1 sm:ring-gray-900/10">
             <div className="-ml-0.5 flex h-16 items-center gap-x-6">
-              <button type="button" className="-m-2.5 p-2.5 text-gray-700" onClick={() => setMobileMenuOpen(false)}>
+              <button
+                type="button"
+                className="-m-2.5 p-2.5 text-gray-700"
+                onClick={() => setMobileMenuOpen(false)}
+              >
                 <span className="sr-only">Close menu</span>
                 <XMarkIcon className="h-6 w-6" aria-hidden="true" />
               </button>
@@ -105,8 +127,8 @@ export default function Settings() {
         </Dialog>
       </header>
 
-      <div className="mx-auto max-w-7xl pt-16 lg:flex lg:gap-x-16 lg:px-8">
-        <aside className="flex overflow-x-auto border-b border-gray-900/5 py-4 lg:block lg:w-64 lg:flex-none lg:border-0 lg:py-20">
+      <div className="mx-auto max-w-7xl lg:flex lg:gap-x-16 lg:px-8 ">
+        {/* <aside className="flex overflow-x-auto border-b border-gray-900/5 py-4 lg:block lg:w-64 lg:flex-none lg:border-0 lg:py-20">
           <nav className="flex-none px-4 sm:px-6 lg:px-0">
             <ul role="list" className="flex gap-x-3 gap-y-1 whitespace-nowrap lg:flex-col">
               {secondaryNavigation.map((item) => (
@@ -133,36 +155,49 @@ export default function Settings() {
               ))}
             </ul>
           </nav>
-        </aside>
+        </aside> */}
 
-        <main className="px-4 py-16 sm:px-6 lg:flex-auto lg:px-0 lg:py-20">
+        <main className="px-4 py-6 sm:px-6 lg:flex-auto lg:px-0 lg:py-24">
           <div className="mx-auto max-w-2xl space-y-16 sm:space-y-20 lg:mx-0 lg:max-w-none">
             <div>
-              <h2 className="text-base font-semibold leading-7 text-gray-900">Profile</h2>
+              <h2 className="text-base font-semibold leading-7 text-gray-900">
+                Profile
+              </h2>
               <p className="mt-1 text-sm leading-6 text-gray-500">
-                This information will be displayed publicly so be careful what you share.
+                This information will be displayed publicly so be careful what
+                you share.
               </p>
 
               <dl className="mt-6 space-y-6 divide-y divide-gray-100 border-t border-gray-200 text-sm leading-6">
                 <div className="pt-6 sm:flex">
-                  <dt className="font-medium text-gray-900 sm:w-64 sm:flex-none sm:pr-6">Full name</dt>
+                  <dt className="font-medium text-gray-900 sm:w-64 sm:flex-none sm:pr-6">
+                    Full name
+                  </dt>
                   <dd className="mt-1 flex justify-between gap-x-6 sm:mt-0 sm:flex-auto">
                     <div className="text-gray-900">Tom Cook</div>
-                    <button type="button" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                    <button
+                      type="button"
+                      className="font-semibold text-indigo-600 hover:text-indigo-500"
+                    >
                       Update
                     </button>
                   </dd>
                 </div>
                 <div className="pt-6 sm:flex">
-                  <dt className="font-medium text-gray-900 sm:w-64 sm:flex-none sm:pr-6">Email address</dt>
+                  <dt className="font-medium text-gray-900 sm:w-64 sm:flex-none sm:pr-6">
+                    Email address
+                  </dt>
                   <dd className="mt-1 flex justify-between gap-x-6 sm:mt-0 sm:flex-auto">
                     <div className="text-gray-900">tom.cook@example.com</div>
-                    <button type="button" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                    <button
+                      type="button"
+                      className="font-semibold text-indigo-600 hover:text-indigo-500"
+                    >
                       Update
                     </button>
                   </dd>
                 </div>
-                <div className="pt-6 sm:flex">
+                {/* <div className="pt-6 sm:flex">
                   <dt className="font-medium text-gray-900 sm:w-64 sm:flex-none sm:pr-6">Title</dt>
                   <dd className="mt-1 flex justify-between gap-x-6 sm:mt-0 sm:flex-auto">
                     <div className="text-gray-900">Human Resources Manager</div>
@@ -170,11 +205,11 @@ export default function Settings() {
                       Update
                     </button>
                   </dd>
-                </div>
+                </div> */}
               </dl>
             </div>
 
-            <div>
+            {/* <div>
               <h2 className="text-base font-semibold leading-7 text-gray-900">Bank accounts</h2>
               <p className="mt-1 text-sm leading-6 text-gray-500">Connect bank accounts to your account.</p>
 
@@ -198,9 +233,9 @@ export default function Settings() {
                   <span aria-hidden="true">+</span> Add another bank
                 </button>
               </div>
-            </div>
+            </div> */}
 
-            <div>
+            {/* <div>
               <h2 className="text-base font-semibold leading-7 text-gray-900">Integrations</h2>
               <p className="mt-1 text-sm leading-6 text-gray-500">Connect applications to your account.</p>
 
@@ -218,35 +253,52 @@ export default function Settings() {
                   <span aria-hidden="true">+</span> Add another application
                 </button>
               </div>
-            </div>
+            </div> */}
 
             <div>
-              <h2 className="text-base font-semibold leading-7 text-gray-900">Language and dates</h2>
+              <h2 className="text-base font-semibold leading-7 text-gray-900">
+                Language and dates
+              </h2>
               <p className="mt-1 text-sm leading-6 text-gray-500">
-                Choose what language and date format to use throughout your account.
+                Choose what language and date format to use throughout your
+                account.
               </p>
 
               <dl className="mt-6 space-y-6 divide-y divide-gray-100 border-t border-gray-200 text-sm leading-6">
                 <div className="pt-6 sm:flex">
-                  <dt className="font-medium text-gray-900 sm:w-64 sm:flex-none sm:pr-6">Language</dt>
+                  <dt className="font-medium text-gray-900 sm:w-64 sm:flex-none sm:pr-6">
+                    Language
+                  </dt>
                   <dd className="mt-1 flex justify-between gap-x-6 sm:mt-0 sm:flex-auto">
                     <div className="text-gray-900">English</div>
-                    <button type="button" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                    <button
+                      type="button"
+                      className="font-semibold text-indigo-600 hover:text-indigo-500"
+                    >
                       Update
                     </button>
                   </dd>
                 </div>
                 <div className="pt-6 sm:flex">
-                  <dt className="font-medium text-gray-900 sm:w-64 sm:flex-none sm:pr-6">Date format</dt>
+                  <dt className="font-medium text-gray-900 sm:w-64 sm:flex-none sm:pr-6">
+                    Date format
+                  </dt>
                   <dd className="mt-1 flex justify-between gap-x-6 sm:mt-0 sm:flex-auto">
                     <div className="text-gray-900">DD-MM-YYYY</div>
-                    <button type="button" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                    <button
+                      type="button"
+                      className="font-semibold text-indigo-600 hover:text-indigo-500"
+                    >
                       Update
                     </button>
                   </dd>
                 </div>
                 <Switch.Group as="div" className="flex pt-6">
-                  <Switch.Label as="dt" className="w-64 flex-none pr-6 font-medium text-gray-900" passive>
+                  <Switch.Label
+                    as="dt"
+                    className="w-64 flex-none pr-6 font-medium text-gray-900"
+                    passive
+                  >
                     Automatic timezone
                   </Switch.Label>
                   <dd className="flex flex-auto items-center justify-end">
@@ -254,15 +306,19 @@ export default function Settings() {
                       checked={automaticTimezoneEnabled}
                       onChange={setAutomaticTimezoneEnabled}
                       className={classNames(
-                        automaticTimezoneEnabled ? 'bg-indigo-600' : 'bg-gray-200',
-                        'flex w-8 cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+                        automaticTimezoneEnabled
+                          ? "bg-indigo-600"
+                          : "bg-gray-200",
+                        "flex w-8 cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                       )}
                     >
                       <span
                         aria-hidden="true"
                         className={classNames(
-                          automaticTimezoneEnabled ? 'translate-x-3.5' : 'translate-x-0',
-                          'h-4 w-4 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out'
+                          automaticTimezoneEnabled
+                            ? "translate-x-3.5"
+                            : "translate-x-0",
+                          "h-4 w-4 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out"
                         )}
                       />
                     </Switch>
@@ -274,7 +330,5 @@ export default function Settings() {
         </main>
       </div>
     </>
-  )
+  );
 }
-
-/* 交流QQ群:七545七三七七八 */
