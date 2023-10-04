29a293fde5124dbc8cbf1d781771b84853ad94a5
Nik Syahmi <niksyahmiirfan01@gmail.com>
2023-10-04 18:53:45 +0800

    Implement Add Guardian functionality

--- client/src/components/AddGuardian.jsx
+++ client/src/components/AddGuardian.jsx
@@ -1,5 +1,121 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
 const AddGuardian = () => {
-  return <div>AddGuardian</div>;
+  const [guardianData, setGuardianData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setGuardianData({
+      ...guardianData,
+      [name]: value,
+    });
+  };
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await axios.post("http://localhost:3001/guardians", guardianData);
+    } catch (error) {
+      console.error("Error adding guardian:", error);
+    }
+  };
+
+  return (
+    <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
+        <div className="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">
+          <h1 className="leading-7 text-gray-900 text-4xl font-bold mb-10">
+            Add Guardian
+          </h1>
+          <form onSubmit={handleFormSubmit}>
+            <div className="border-b border-gray-900/10 pb-12">
+              <label
+                htmlFor="name"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Name
+              </label>
+              <div className="mt-2">
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  value={guardianData.name}
+                  onChange={handleInputChange}
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  required
+                />
+              </div>
+
+              <div className="col-span-full mt-10">
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Email
+                </label>
+                <div className="mt-2">
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    value={guardianData.email}
+                    onChange={handleInputChange}
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    required
+                  />
+                </div>
+              </div>
+
+              <div className="mt-10">
+                <label
+                  htmlFor="phone"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Phone
+                </label>
+                <div className="mt-2">
+                  <input
+                    type="text"
+                    id="phone"
+                    name="phone"
+                    value={guardianData.phone}
+                    onChange={handleInputChange}
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    required
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className="mt-6 flex items-center justify-end gap-x-6">
+              <Link to={"/dashboard"}>
+                <button
+                  type="button"
+                  className="text-sm font-semibold leading-6 text-gray-900"
+                >
+                  Cancel
+                </button>
+              </Link>
+              <button
+                type="submit"
+                className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Save
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AddGuardian;
