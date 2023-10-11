85de0cba71409d09d75a21163d65c57b33f8c499
nikumaruku <74139509+nikumaruku@users.noreply.github.com>
2023-10-11 23:20:24 +0800

    Initialise Admin dashboard

---
+++ client/src/pages/admin/UserStats.jsx
@@ -0,0 +1,30 @@
+const stats = [
+  { name: "Total Subscribers", stat: "71,897" },
+  { name: "Avg. Open Rate", stat: "58.16%" },
+  { name: "Avg. Click Rate", stat: "24.57%" },
+];
+
+export default function UserStats() {
+  return (
+    <div className="mb-8">
+      <h3 className="text-base font-semibold leading-6 text-gray-900">
+        Stats of Users
+      </h3>
+      <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
+        {stats.map((item) => (
+          <div
+            key={item.name}
+            className="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6"
+          >
+            <dt className="truncate text-sm font-medium text-gray-500">
+              {item.name}
+            </dt>
+            <dd className="mt-1 text-3xl font-semibold tracking-tight text-gray-900">
+              {item.stat}
+            </dd>
+          </div>
+        ))}
+      </dl>
+    </div>
+  );
+}
