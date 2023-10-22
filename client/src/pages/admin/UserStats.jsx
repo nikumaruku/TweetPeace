9815d58c88def50a4d72319ea33520ccf61d864d
Nik Syahmi <niksyahmiirfan01@gmail.com>
2023-10-22 14:04:05 +0800

    Use charts to visualise user stats

--- client/src/pages/admin/UserStats.jsx
+++ client/src/pages/admin/UserStats.jsx
@@ -1,30 +1,60 @@
+import { useEffect, useRef } from "react";
+import Chart from "chart.js/auto";
+
 const stats = [
   { name: "Registered Users", stat: "3" },
   { name: "Active users", stat: "1" },
-  // { name: "Number ", stat: "24.57%" },
+  { name: "Unique Visitors", stat: "15" },
 ];
 
 export default function UserStats() {
+  const chartRef = useRef(null);
+  const chartInstance = useRef(null);
+
+  useEffect(() => {
+    if (chartRef.current) {
+      const ctx = chartRef.current.getContext("2d");
+
+      if (chartInstance.current) {
+        chartInstance.current.destroy();
+      }
+
+      const labels = stats.map((item) => item.name);
+      const data = stats.map((item) => Number(item.stat));
+
+      chartInstance.current = new Chart(ctx, {
+        type: "bar",
+        data: {
+          labels: labels,
+          datasets: [
+            {
+              label: "Statistics",
+              data: data,
+              backgroundColor: ["#63B3ED", "#93C5FD", "#A5B4FC"],
+            },
+          ],
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true,
+            },
+          },
+          responsive: true,
+          maintainAspectRatio: false,
+        },
+      });
+    }
+  }, []);
+
   return (
     <div className="mb-8">
       <h3 className="text-base font-semibold leading-6 text-gray-900">
         User Statistics
       </h3>
-      <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
-        {stats.map((item) => (
-          <div
-            key={item.name}
-            className="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6"
-          >
-            <dt className="truncate text-sm font-medium text-gray-500">
-              {item.name}
-            </dt>
-            <dd className="mt-1 text-3xl font-semibold tracking-tight text-gray-900">
-              {item.stat}
-            </dd>
-          </div>
-        ))}
-      </dl>
+      <div className="mt-5 bg-white shadow-xl p-8 rounded-xl">
+        <canvas ref={chartRef} />
+      </div>
     </div>
   );
 }
