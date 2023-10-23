3004fcee2863795637bda155eaac6cab7d5ff465
nikumaruku <74139509+nikumaruku@users.noreply.github.com>
2023-10-24 00:28:22 +0800

    Implement guardian details pop up

--- client/src/pages/admin/RegisteredUsers.jsx
+++ client/src/pages/admin/RegisteredUsers.jsx
@@ -1,15 +1,32 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { EnvelopeIcon, UserGroupIcon } from "@heroicons/react/20/solid";
+import UserPopUp from "./UserPopUp";
 
 export default function RegisteredUsers() {
   const [userDatas, setUserDatas] = useState(null);
+  const [isUserPopUpOpen, setUserPopUpOpen] = useState(false);
+  const [selectedUser, setSelectedUser] = useState(null);
+  const [selectedGuardians, setSelectedGuardians] = useState([]);
 
   useEffect(() => {
     axios.get(`http://localhost:3001/users`).then((response) => {
       setUserDatas(response.data);
     });
   }, []);
+
+  const fetchGuardians = (username) => {
+    axios
+      .get(`http://localhost:3001/guardians/obtain/${username}`)
+      .then((response) => {
+        setSelectedGuardians(response.data);
+        setSelectedUser(username);
+        setUserPopUpOpen(true);
+      })
+      .catch((error) => {
+        console.error("Error fetching guardian details:", error);
+      });
+  };
 
   const mappedUserDatas = userDatas
     ? userDatas.map((user) => ({
@@ -23,64 +40,74 @@ export default function RegisteredUsers() {
     : [];
 
   return (
-    <ul
-      role="list"
-      className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
-    >
-      {mappedUserDatas.map((person) => (
-        <li
-          key={person.email}
-          className="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-white text-center shadow"
-        >
-          <div className="flex flex-1 flex-col p-8">
-            <img
-              className="mx-auto h-32 w-32 flex-shrink-0 rounded-full"
-              src={person.imageUrl}
-              alt={person.name}
-            />
+    <div>
+      {isUserPopUpOpen && (
+        <UserPopUp
+          user={selectedUser}
+          guardians={selectedGuardians}
+          closeUserPopUp={() => setUserPopUpOpen(false)}
+        />
+      )}
 
-            <h3 className="mt-6 text-sm font-medium text-gray-900">
-              {person.name}
-            </h3>
-            <dl className="mt-1 flex flex-grow flex-col justify-between">
-              <dt className="sr-only">Title</dt>
-              <dd className="text-sm text-gray-500">{person.title}</dd>
-              <dt className="sr-only">Role</dt>
-              <dd className="mt-3">
-                <span className="inline-flex items-center rounded-full bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
-                  {person.role}
-                </span>
-              </dd>
-            </dl>
-          </div>
-          <div>
-            <div className="-mt-px flex divide-x divide-gray-200">
-              <div className="flex w-0 flex-1">
-                <a
-                  href={`mailto:${person.email}`}
-                  className="relative -mr-px inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-bl-lg border border-transparent py-4 text-sm font-semibold text-gray-900"
-                >
-                  <EnvelopeIcon
-                    className="h-5 w-5 text-gray-400"
-                    aria-hidden="true"
-                  />
-                </a>
-              </div>
-              <div className="-ml-px flex w-0 flex-1">
-                <a
-                  href={`#`}
-                  className="relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-br-lg border border-transparent py-4 text-sm font-semibold text-gray-900"
-                >
-                  <UserGroupIcon
-                    className="h-5 w-5 text-gray-400"
-                    aria-hidden="true"
-                  />
-                </a>
+      <ul
+        role="list"
+        className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
+      >
+        {mappedUserDatas.map((person) => (
+          <li
+            key={person.email}
+            className="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-white text-center shadow"
+          >
+            <div className="flex flex-1 flex-col p-8">
+              <img
+                className="mx-auto h-32 w-32 flex-shrink-0 rounded-full"
+                src={person.imageUrl}
+                alt={person.name}
+              />
+
+              <h3 className="mt-6 text-sm font-medium text-gray-900">
+                {person.name}
+              </h3>
+              <dl className="mt-1 flex flex-grow flex-col justify-between">
+                <dt className="sr-only">Title</dt>
+                <dd className="text-sm text-gray-500">{person.title}</dd>
+                <dt className="sr-only">Role</dt>
+                <dd className="mt-3">
+                  <span className="inline-flex items-center rounded-full bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
+                    {person.role}
+                  </span>
+                </dd>
+              </dl>
+            </div>
+            <div>
+              <div className="-mt-px flex divide-x divide-gray-200">
+                <div className="flex w-0 flex-1">
+                  <a
+                    href={`mailto:${person.email}`}
+                    className="relative -mr-px inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-bl-lg border border-transparent py-4 text-sm font-semibold text-gray-900"
+                  >
+                    <EnvelopeIcon
+                      className="h-5 w-5 text-gray-400"
+                      aria-hidden="true"
+                    />
+                  </a>
+                </div>
+                <div className="-ml-px flex w-0 flex-1">
+                  <a
+                    onClick={() => fetchGuardians(person.name)}
+                    className="relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-br-lg border border-transparent py-4 text-sm font-semibold text-gray-900"
+                  >
+                    <UserGroupIcon
+                      className="h-5 w-5 text-gray-400"
+                      aria-hidden="true"
+                    />
+                  </a>
+                </div>
               </div>
             </div>
-          </div>
-        </li>
-      ))}
-    </ul>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
--- client/src/pages/admin/UserPopUp.jsx
+++ client/src/pages/admin/UserPopUp.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
-
-const UserPopUp = () => {
+const UserPopUp = ({ user, guardians, closeUserPopUp }) => {
   return (
-    <div>UserPopUp</div>
-  )
-}
+    <div>
+      <div className="fixed inset-0 flex items-center justify-center z-50">
+        <div className="bg-white border-2 border-black rounded-lg p-6 w-96 shadow-xl">
+          <div className="flex justify-end">
+            <button
+              onClick={() => closeUserPopUp()}
+              className="text-gray-500 hover:text-gray-700 focus:outline-none"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </div>
+          <h2 className="text-lg font-semibold mb-4">{user}'s Guardian</h2>
 
-export default UserPopUp
+          <div className="mb-4">
+            <ul>
+              {guardians.map((guardian, index) => (
+                <li key={index}>
+                  <div>
+                    <strong>Name:</strong> {guardian.name}
+                  </div>
+                  <div>
+                    <strong>Email:</strong> {guardian.email}
+                  </div>
+                  <div>
+                    <strong>Phone:</strong> {guardian.phone}
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserPopUp;
