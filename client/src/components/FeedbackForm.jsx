47e4b307a924a44db86643ef6bd58fdb75e99e55
nikumaruku <74139509+nikumaruku@users.noreply.github.com>
2023-10-25 23:52:23 +0800

    Visual changes at feedback form

--- client/src/components/FeedbackForm.jsx
+++ client/src/components/FeedbackForm.jsx
@@ -45,7 +45,7 @@ const FeedbackForm = ({ setIsFeedbackOpen, tweetId }) => {
 
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50">
-      <div className="bg-white rounded-lg p-6 w-96">
+      <div className="bg-white shadow-xl border-2 border-black rounded-lg p-6 w-96">
         <div className="flex justify-end">
           <button
             onClick={() => setIsFeedbackOpen(false)}
