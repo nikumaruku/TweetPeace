d34f32814edf989c1c96c1f4e96995df6925e6ce
nikumaruku <74139509+nikumaruku@users.noreply.github.com>
2024-01-11 00:05:27 +0800

    Fix issue when obtaining multiple tweets from a thread

--- server/module/fetchTwitterThread.js
+++ server/module/fetchTwitterThread.js
@@ -1,16 +1,17 @@
 import puppeteer from "puppeteer";
 
 const fetchThreadContent = async (threadUrls) => {
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   const tweetContents = [];
 
   try {
     for (const url of threadUrls) {
-      await page.goto(url, { waitUntil: "networkidle2" });
+      await page.goto(url, { waitUntil: "networkidle2", timeout: 0 });
 
       const threadAnalysis = await page.$$eval(
         "article div[lang]",
+
         (tweetElements) => {
           return tweetElements.map((tweetElement) => tweetElement.textContent);
         }
@@ -20,10 +21,11 @@ const fetchThreadContent = async (threadUrls) => {
     }
     return tweetContents;
   } catch (error) {
-    console.log(error);
+    console.error(error);
+    return [];
+  } finally {
+    await browser.close();
   }
-
-  browser.close();
 };
 
 export default fetchThreadContent;
